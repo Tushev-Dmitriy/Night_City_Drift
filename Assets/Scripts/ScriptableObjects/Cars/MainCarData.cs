10c68c193328a65b32c05e754587544404882252
Tushev-Dmitriy <110030309+Tushev-Dmitriy@users.noreply.github.com>
2025-02-18 15:37:13 +0500

    add test model of vaz 2107

---
+++ Assets/Scripts/ScriptableObjects/Cars/MainCarData.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCar", menuName = "Car")]
+public class MainCarData : ScriptableObject
+{
+    public string carName;
+    public int carPrice;
+    public CarCharacteristics carCharacteristics;
+    public CarView carView;
+}
+
+[Serializable]
+public class CarCharacteristics
+{
+    public int maxSpeed = 120;
+    public int engineLvl = 5;
+    public int steeringAngleLvl = 25;
+    public int brakeLvl = 300;
+    public bool haveNitro = false;
+    public bool haveTurbine = false;
+}
+
+[Serializable]
+public class CarView
+{
+    public string carPlate = "A123AA";
+    public Color carColor;
+    public TypeOfBody bodyType;
+    public Color wheelColor;
+}
+
+public enum TypeOfBody
+{
+    Hatchback, //хэтч
+    Saloon, //седан
+    Coupe, //купе
+    Convertible, //кабриолет
+    Liftback, //лифтбэк
+}
