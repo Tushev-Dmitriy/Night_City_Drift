94317bec2bf0adb76ee10d079c8d98c6c76e995f
Tushev-Dmitriy <110030309+Tushev-Dmitriy@users.noreply.github.com>
2025-03-04 13:51:09 +0500

    test traffic

---
+++ Assets/ScriptableObjects/Events/MaterialGameEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MaterialGameEvent", menuName = "GameEvents")]
+public class MaterialGameEvent : BaseGameEvent<Material>
+{
+}
---
+++ Assets/ScriptableObjects/Events/MaterialGameEventListener.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MaterialGameEventListener : BaseGameEventListener<Material, MaterialGameEvent, UnityEvent<Material>>
+{
+}
